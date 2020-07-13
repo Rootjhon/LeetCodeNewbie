2e0fea2cfb1b930b508f8e6340d63792b7ef20e9
JunQiangZhu <354888562@qq.com>
2020-07-13 23:51:56 +0800

    删除最外层的括号

---
+++ leetcode/stack/RemoveOuterParentheses.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace leetcode
+{
+    //https://leetcode-cn.com/problems/remove-outermost-parentheses/
+    public partial class Solution
+    {
+        public string RemoveOuterParentheses(string S)
+        {
+            Console.WriteLine(S);
+            return String.Empty;
+        }
+    }
+}
