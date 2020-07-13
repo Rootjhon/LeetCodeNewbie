1ca486217eab5bea0510068e36d13ac747560ed0
Rootjhon <354888562@qq.com>
2020-07-13 14:15:30 +0800

    1

---
+++ leetcode/Entrance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace leetcode
+{
+    class Entrance
+    {
+        private static Stopwatch _watch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            var tempP = new Intersect_Solution();
+
+            _watch.Start();
+            tempP.Intersect_V2(new int[] {4,9,5 },new int[] { 9, 4, 9, 8, 4 });
+            _watch.Stop();
+
+            Console.WriteLine(_watch.ElapsedMilliseconds);
+        }
+    }
+}
