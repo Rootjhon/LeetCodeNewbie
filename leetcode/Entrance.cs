ef6552ec694f413d348faf7ad84a5aa8352e60cc
Rootjhon <354888562@qq.com>
2020-07-14 14:41:44 +0800

    CountNegatives

--- leetcode/Entrance.cs
+++ leetcode/Entrance.cs
@@ -8,7 +8,7 @@ namespace leetcode
         #region [Fields]
         private static Stopwatch _watch = new Stopwatch();
         #endregion
-        
+
         private static void Main(string[] args)
         {
             var tempP = new Solution();
@@ -16,7 +16,7 @@ namespace leetcode
             _watch.Start();
             //tempP.Intersect_v2(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 });
             //tempP.RemoveOuterParentheses_v1("(()())(())");
-            tempP.RemoveOuterParentheses_v1("(()())(())(()(()))");//"()()()()(())";
+            //tempP.RemoveOuterParentheses_v1("(()())(())(()(()))");//"()()()()(())";
             //tempP.RemoveOuterParentheses_v1("()()");//"()()()()(())";
             _watch.Stop();
 
---
+++ leetcode/easy/CountNegatives.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode
+{
+    public partial class Solution
+    {
+        public int CountNegatives_v0(int[][] grid)
+        {
+            var tempCount = 0;
+            for (int iR = 0; iR < grid.Length; ++iR)
+            {
+                var tempRow = grid[iR];
+                for (int iC = 0; iC < tempRow.Length; ++iC)
+                {
+                    if (tempRow[iC] < 0)
+                    {
+                        tempCount++;
+                    }
+                }
+            }
+            return tempCount;
+        }
+
+        public int CountNegatives_v1(int[][] grid)
+        {
+            var tempCount = 0;
+            var tempNegaRaw = grid.Length;
+            var tempNegaCol = grid[0].Length;
+            for (int iR = 0; iR < tempNegaRaw; ++iR)
+            {
+                var tempRow = grid[iR];
+                for (int iC = 0; iC < tempNegaCol; ++iC)
+                {
+                    if (tempRow[iC] < 0)
+                    {
+                        tempCount += (grid.Length - iR) * (tempNegaCol - iC);
+                        tempNegaCol = iC;
+                        break;
+                    }
+                }
+            }
+            return tempCount;
+        }
+    }
+}
