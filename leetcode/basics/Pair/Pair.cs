5169e583a43a0faaa056e31b134d2f752a134e52
Rootjhon <354888562@qq.com>
2020-07-23 20:17:24 +0800

    最短路径

--- leetcode/basics/Pair/Pair.cs
+++ leetcode/basics/Pair/Pair.cs
@@ -1,35 +1,32 @@
-﻿namespace leetcode.basics
+﻿public sealed class Pair<TFirst, TSecond>
 {
-    public sealed class Pair<TFirst, TSecond>
+    #region [Fields]
+    public TFirst First { get; set; }
+    public TSecond Second { get; set; }
+    #endregion
+
+    #region [Construct]
+    public Pair() { }
+    public Pair(TFirst varFirst, TSecond varSecond)
     {
-        #region [Fields]
-        public TFirst First { get; set; }
-        public TSecond Second { get; set; }
-        #endregion
-
-        #region [Construct]
-        public Pair() { }
-        public Pair(TFirst varFirst, TSecond varSecond)
-        {
-            First = varFirst;
-            Second = varSecond;
-        }
-        #endregion
-
-        #region [Business]
-        public override int GetHashCode()
-        {
-            return (First.GetHashCode() << 5) ^ Second.GetHashCode();
-        }
-        public bool Equals(Pair<TFirst, TSecond> varOther)
-        {
-            return First.Equals(varOther.First) && Second.Equals(varOther.Second);
-        }
-        public override bool Equals(object varOther)
-        {
-            var otherPair = varOther as Pair<TFirst, TSecond>;
-            return (otherPair != null && Equals(otherPair));
-        }
-        #endregion
+        First = varFirst;
+        Second = varSecond;
     }
+    #endregion
+
+    #region [Business]
+    public override int GetHashCode()
+    {
+        return (First.GetHashCode() << 5) ^ Second.GetHashCode();
+    }
+    public bool Equals(Pair<TFirst, TSecond> varOther)
+    {
+        return First.Equals(varOther.First) && Second.Equals(varOther.Second);
+    }
+    public override bool Equals(object varOther)
+    {
+        var otherPair = varOther as Pair<TFirst, TSecond>;
+        return (otherPair != null && Equals(otherPair));
+    }
+    #endregion
 }
